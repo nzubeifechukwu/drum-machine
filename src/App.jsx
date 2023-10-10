4c787e6c32e1f7997fa6597b467cd59ef933b76b
Nzubechukwu Ifechukwu <nzube.ifechukwu@gmail.com>
2023-10-10 19:41:57 +0100

    Add drum machine files

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,135 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { keys } from "./utils/keys";
 
 function App() {
   const [audioName, setAudioName] = useState("");
+  const [isOn, setIsOn] = useState(true);
+  const [volume, setVolume] = useState(50);
 
   const playAudioOnClick = (e, url) => {
     const audio = new Audio(url);
     audio.play();
-    // console.log(e.target.innerText);
     const clickedKey = keys.find((key) => key.key === e.target.innerText);
     setAudioName(clickedKey.audioName);
+    audio.volume = volume / 100;
   };
 
-  const playAudioOnKeyDown = (e) => {
-    console.log(e.key);
-    const pressedKey = keys.find((key) => key.key === e.key.toUpperCase());
-    const audio = new Audio(pressedKey?.audioUrl);
-    audio.play();
-    setAudioName(pressedKey?.audioName);
-  };
-
-  return (
-    <div>
-      {keys.map((key) => {
-        return (
-          <button
-            key={key.id}
-            onKeyDown={(e) => playAudioOnKeyDown(e)}
-            onClick={(e) => playAudioOnClick(e, key.audioUrl)}
-          >
-            {key.key}
-          </button>
-        );
-      })}
-      <p>{audioName}</p>
-    </div>
+  const playAudioOnKeyDown = useCallback(
+    (e) => {
+      const pressedKey = keys.find((key) => key.key === e.key.toUpperCase());
+      const audio = new Audio(pressedKey?.audioUrl);
+      audio.play();
+      setAudioName(pressedKey?.audioName);
+      audio.volume = volume / 100;
+    },
+    [volume]
   );
+
+  useEffect(() => {
+    if (isOn) {
+      document.body.addEventListener("keydown", playAudioOnKeyDown);
+
+      return () => {
+        document.body.removeEventListener("keydown", playAudioOnKeyDown);
+      };
+    }
+  }, [isOn, playAudioOnKeyDown]);
+
+  if (isOn) {
+    return (
+      <>
+        <main>
+          <section className="keys">
+            {keys.map((key) => {
+              return (
+                <button
+                  key={key.id}
+                  onClick={(e) => playAudioOnClick(e, key.audioUrl)}
+                >
+                  {key.key}
+                </button>
+              );
+            })}
+          </section>
+          <section className="controls">
+            <div className="power">
+              <p>Power</p>
+              <button
+                onClick={() => {
+                  setIsOn(!isOn);
+                  setAudioName("");
+                }}
+                style={{ backgroundColor: "#06d6a0" }}
+              >
+                {isOn && "on"}
+              </button>
+            </div>
+            <div className="audio-name">
+              <p>{audioName}</p>
+            </div>
+            <div className="volume">
+              <p>volume</p>
+              <input
+                type="range"
+                min={0}
+                max={100}
+                value={volume}
+                onChange={(e) => {
+                  setVolume(e.target.valueAsNumber);
+                }}
+              />
+            </div>
+          </section>
+        </main>
+        <footer>
+          <a href="https://github.com/nzubeifechukwu/drum-machine">
+            &copy; Nzube Ifechukwu
+          </a>
+        </footer>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <main>
+          <section className="keys">
+            {keys.map((key) => {
+              return <button key={key.id}>{key.key}</button>;
+            })}
+          </section>
+          <section className="controls">
+            <div className="power">
+              <p>Power</p>
+              <button
+                onClick={() => setIsOn(!isOn)}
+                style={{ backgroundColor: "#ef476f" }}
+              >
+                {isOn || "off"}
+              </button>
+            </div>
+            <div className="audio-name"></div>
+            <div className="volume">
+              <p>volume</p>
+              <input
+                type="range"
+                min={0}
+                max={100}
+                value={volume}
+                onChange={(e) => {
+                  setVolume(e.target.valueAsNumber);
+                }}
+              />
+            </div>
+          </section>
+        </main>
+        <footer>
+          <a href="https://github.com/nzubeifechukwu/drum-machine">
+            &copy; Nzube Ifechukwu
+          </a>
+        </footer>
+      </>
+    );
+  }
 }
 
 export default App;
